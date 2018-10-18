e411fbe3eb817d59db218511a9bc610ca8578cc8
Tom Chan <ittomchan@gmail.com>
2018-10-18 14:01:09 +0800

    sumbit questions

---
+++ Day 15/test.js
@@ -0,0 +1,17 @@
+var car = {
+    brands: {
+        BMW: {
+            6-series: {
+                sold: 1805
+            }
+        },
+        Tesla: {
+            Model-S: {
+                sold: 200
+            },
+            "Model-A": {
+                sold: 14
+            },
+        }
+    }
+}
